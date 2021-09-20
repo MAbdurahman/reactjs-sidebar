cff8a087985eb56684671d8eaf664c1d2e60e8b8
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-20 15:46:46 -0500

    Add a logo

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,12 +2,43 @@ import React from 'react'
 import { FaTimes } from 'react-icons/fa';
 import { useGlobalContext } from './../../utils/context';
 import { social, links } from './../../utils/data'
-import logo from './../../img/logo.svg'
+import logo from './../../img/logo.png'
 
 export default function Sidebar() {
    return (
-      <div>
-         <h2>this is sideBar</h2>
-      </div>
-   )
+		<aside
+			className={`sidebar show-sidebar`}
+		>
+			<div className='sidebar-header'>
+				<img src={logo} className='logo' alt='pro-tech' width='250px' height='auto'/> 
+            
+				<button className='close-btn'>
+					<FaTimes />
+				</button>
+			</div>
+			<ul className='links'>
+				{links.map(link => {
+					const { id, url, text, icon } = link;
+					return (
+						<li key={id}>
+							<a href={url}>
+								{icon}
+								{text}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+			<ul className='social-icons'>
+				{social.map(link => {
+					const { id, url, icon } = link;
+					return (
+						<li key={id}>
+							<a href={url}>{icon}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</aside>
+	);
 }
