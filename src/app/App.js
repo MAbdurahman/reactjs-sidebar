d220e8816423746a23c60ea7cfe77d06bd1e2649
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-20 14:08:16 -0500

    Add website reset and google fonts

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h2>This is the application</h2>
     </div>
   );
